0ae27c843258713d18d4c7863f05ddbd51dccd8c
Anurag <code.annu@gmail.com>
2026-09-02 16:35:14 +0530

    fix database url according to node_env

--- packages/config/src/env/load.ts
+++ packages/config/src/env/load.ts
@@ -1,10 +1,12 @@
 import { config } from "dotenv";
 
 config();
+const NODE_ENV = process.env.NODE_ENV;
+const DB_NAME = `${process.env.DB_NAME}-${NODE_ENV}`;
 
 const ENV = {
   PORT: Number(process.env.PORT),
-  DATABASE_URL: process.env.DATABASE_URL!,
+  DATABASE_URL: `${process.env.DATABASE_URL}/${DB_NAME}`,
   ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET!,
   ACCESS_TOKEN_EXPIRY_MIN: Number(process.env.ACCESS_TOKEN_EXPIRY_MIN),
   REFRESH_TOKEN_EXPIRY_DAYS: Number(process.env.REFRESH_TOKEN_EXPIRY_DAYS),
